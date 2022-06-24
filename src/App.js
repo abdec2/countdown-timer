1f43a62d808eca35c4f9d031ad0b123d950ba468
abdec2 <abdec2@hotmail.com>
2022-06-24 21:46:59 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Countdown, { zeroPad } from "react-countdown";
 
 function App() {
+  const mintDate = new Date("2022-06-25T00:00:00");
+
+  const renderer = ({ days, hours, minutes, seconds }) => (
+    <div className="flex items-center">
+      <div className="bg-black bg-opacity-40 rounded-lg m-[10px] min-w-[100px] py-5 text-center border border-white">
+        <div className="text-white text-[2rem] mb-[10px]">{zeroPad(days)}</div>
+        <div className="uppercase text-white">Days</div>
+      </div>
+      <div className="bg-black bg-opacity-40 rounded-lg m-[10px] min-w-[100px] py-5 text-center border border-white">
+        <div className="text-white text-[2rem] mb-[10px]">{zeroPad(hours)}</div>
+        <div className="uppercase text-white">Hours</div>
+      </div>
+      <div className="bg-black bg-opacity-40 rounded-lg m-[10px] min-w-[100px] py-5 text-center border border-white">
+        <div className="text-white text-[2rem] mb-[10px]">{zeroPad(minutes)}</div>
+        <div className="uppercase text-white">Mins</div>
+      </div>
+      <div className="bg-black bg-opacity-40 rounded-lg m-[10px] min-w-[100px] py-5 text-center border border-white">
+        <div className="text-white text-[2rem] mb-[10px]">{zeroPad(seconds)}</div>
+        <div className="uppercase text-white">Secs</div>
+      </div>
+     
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <div className="flex items-center justify-center">
+        <Countdown
+          date={Date.now() + mintDate.getTime() - new Date().getTime()}
+          renderer={renderer}
+        />
+      </div>
     </div>
   );
 }
